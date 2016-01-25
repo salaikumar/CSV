29911abc7af18104acf1ef900e41d589f380fb2a
Salaikumar <saravanan1115@gmail.com>
2016-01-25 20:23:14 +0530

    CSV Parser API Defined

--- src/main/java/me/salaikumar/csv/CSVParser.java
+++ src/main/java/me/salaikumar/csv/CSVParser.java
@@ -1,10 +1,44 @@
 package me.salaikumar.csv;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * CSV Parser
  * An Attempt to create a Standard CSV Parser in Java
  */
 public class CSVParser {
 
+    private final CSVFormat csvFormat;
+    private List<Record> records;
+
+    public CSVParser(CSVFormat csvFormat) {
+        this.csvFormat = csvFormat;
+    }
+
+    public CSVParser parse(String fileLocation){
+        return null;
+    }
+
+    public List<Record> getAllRecords(){
+        return null;
+    }
+
+    public Record getRecord(){
+        return null;
+    }
+
+    public Record getHeaderRecord(){
+        return null;
+    }
+
+    /*
+     * At Attempt to provide Querying capability on CSV files
+     * Works with both Header and Non Header CSV files
+     * If it works successfull, Try out CRUD on CSV
+     */
+    public List<Record> find(Map query ){
+        return null;
+    }
 
 }
---
+++ src/main/java/me/salaikumar/csv/Record.java
@@ -0,0 +1,7 @@
+package me.salaikumar.csv;
+
+/**
+ * Created by salaikumar on 25/1/16.
+ */
+public class Record {
+}
